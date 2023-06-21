a141cb6af43371e2808635d9562fb17f0bfd5495
sekkakidriss <125156491+sekkakidriss@users.noreply.github.com>
2023-06-21 14:51:30 -0400

    Delete RealdataF.m

--- RealdataF.m
+++
@@ -1,7 +0,0 @@
-function [Sr,Ir,Qr,Rr]=RealdataF(n)
-  Sr=xlsread('calibrationdraft.xlsx','Sheet3','N2:N92');
-  Ir=xlsread('calibrationdraft.xlsx','Sheet3','O2:O92');
-  %Qr=xlsread('calibrationdraft.xlsx','Sheet3','N2:N62');
-  Qr=zeros(n,1);
-  Rr=xlsread('calibrationdraft.xlsx','Sheet3','L2:L92');
-end
